ae57c370ac55532f3657622b67c7977e7b322738
AChung1020 <achun46@emory.edu>
2023-07-23 15:28:39 -0400

    First Commit

---
+++ venv/src/Discord/cogs/help_cog.py
@@ -0,0 +1,49 @@
+import discord
+from discord.ext import commands
+
+
+class help_cog(commands.Cog):
+    def __init__(self, bot):
+        self.bot = bot
+
+        self.help_message = """
+        ```
+        General Commands:
+        /help - list of commands if you forget you baka ~~~
+        /p or /play [URL] or [search words] - play a song!
+        /q or /queue - see whats playing next!
+        /skip - skip song
+        /clear - clear all songs
+        /dc - kick me from the channel onii chan ~~~
+        /pause - pauses song
+        /resume - you get the point
+        /now_playing or /np - shows what is currently playing
+
+        To Start, use /play with a link from youtube
+        (VIDEO SHOULD NOT BE IN A PLAYLIST)
+        or search keywords
+        ```
+        """
+        self.text_channel_text = []
+
+    # @commands.Cog.listener()
+    # async def on_ready(self):  # specifies that this function will run whenever music bot come online(listener)
+    #     for guild in self.bot.guilds:
+    #         for channel in guild.text_channels:
+    #             self.text_channel_text.append(channel)
+    #
+    #     await self.send_to_all(self.help_message)
+
+    async def send_to_all(self, msg):
+        for text_channel in self.text_channel_text:
+            await text_channel.send(msg)
+
+    @commands.command(name='help', help='Displays all available commands')
+    async def help(self, ctx):
+        await ctx.send(self.help_message)
+
+
+async def setup(bot):
+    await bot.add_cog(help_cog(bot))
+
+
---
+++ venv/src/main.py
@@ -0,0 +1,38 @@
+import discord
+from discord.ext import commands
+import os
+import asyncio
+from dotenv import load_dotenv
+
+load_dotenv()
+
+intents = discord.Intents.default()
+intents.message_content = True
+intents.messages = True
+intents.voice_states = True
+
+bot = commands.Bot(command_prefix='/', intents=intents, help_command=None)
+
+
+@bot.event
+async def on_ready():
+    print("Bot is ready")
+    activity = discord.Activity(type=discord.ActivityType.playing, name="TESTING! DON'T USE")
+    await bot.change_presence(activity=activity)
+
+
+async def load():
+    for filename in os.listdir("Discord/cogs"):
+        if filename.endswith(".py"):
+            await bot.load_extension(f"Discord.cogs.{filename[:-3]}")
+            # use dots for directories no / for this^^
+
+
+async def main():
+    async with bot:
+        await load()
+        await bot.start(os.getenv("DISCORD_TOKEN"))
+
+asyncio.run(main())
+
+
