122c03e2fa532e6c0a83f027c216afd1e41a0b35
AChung1020 <achun46@emory.edu>
2023-07-25 18:19:56 -0400

    Global db variable for ease of access

--- venv/src/MongoDB/connect_database.py
+++ venv/src/MongoDB/connect_database.py
@@ -3,9 +3,19 @@ from dotenv import load_dotenv
 
 load_dotenv()
 
+db = None
+
 
 def get_mongo_client():
+    global db
+
+    if db is not None:
+        return
     client = pymongo.MongoClient("MONGO_URI")
 
     db = client.Discord_Music_db
     print("Current database name:", db)
+    return db
+
+
+print(db)
