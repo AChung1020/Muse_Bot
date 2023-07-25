a32d6c2b167b77befea6deda7d8867b125725c93
AChung1020 <achun46@emory.edu>
2023-07-25 19:38:48 -0400

    initial muse_bot settings on server

---
+++ venv/src/MongoDB/InitMusicSesSettings.py
@@ -0,0 +1,20 @@
+from MongoDB.connect_database import db
+
+session = {
+    "is_Playing": False,
+    "is_Paused": False,
+    # current song
+    "current_track": None,
+    # person who queued current song
+    "current_person": None,
+    "music_queue": [],
+    # settings for optimal audio
+    "vc": None
+}
+
+# test post function
+# def post():
+#     collection = db.get_collection("music_sessions")
+#     print(f"This is the collection: {collection}")
+#     post_id = collection.insert_one(session).inserted_id
+#     print("Inserted document ID:", post_id)
