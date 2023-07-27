28481973062b7a372b7f1be65d54db45d110e2c7
AChung1020 <achun46@emory.edu>
2023-07-27 11:10:06 -0400

    All commands now work with MongoDB

--- venv/src/Discord/cogs/SkipAndRem_cog.py
+++ venv/src/Discord/cogs/SkipAndRem_cog.py
@@ -3,6 +3,7 @@ import asyncio
 from discord.ext import commands
 from Database.RetAndUpdateFunctions import find_document, music_sessions
 
+
 # TODO: fix remove to align with database
 class SkipAndRem_cog(commands.Cog):
     def __init__(self, bot):
@@ -25,15 +26,15 @@ class SkipAndRem_cog(commands.Cog):
             queue_length = len(music_queue)
             if num_members > 2:
                 vc.stop()  # stop playing current song
-                document['is_Playing'] = False
-                document['is_Paused'] = False
 
                 await ctx.send(f"The song: **__{document['current_track']}__** has been skipped.")
                 if queue_length == 0:
                     document['current_track'] = None
                     document['current_person'] = None
+                    document['is_Playing'] = False
+                    document['is_Paused'] = False
+                    collection.update_one({"_id": server_ID}, {"$set": document})
 
-                collection.update_one({"_id": server_ID}, {"$set": document})
                 await self.play_next(vc, find_document(server_ID), server_ID)  # move next
             elif num_members <= 2:
                 message = await ctx.send(
@@ -64,8 +65,6 @@ class SkipAndRem_cog(commands.Cog):
 
                     if Check_Count >= num_success_vote:
                         vc.stop()  # stop playing current song
-                        document['is_Playing'] = False
-                        document['is_Paused'] = False
 
                         await ctx.send(f"The song: **__{document['current_track']}__** has been skipped.\n"
                                        f" Who put this shit song here? \n"
@@ -74,8 +73,8 @@ class SkipAndRem_cog(commands.Cog):
                         if queue_length == 0:
                             document['current_track'] = None
                             document['current_person'] = None
-
-                        collection.update_one({"_id": server_ID}, {"$set": document})
+                            document['is_Playing'] = False
+                            collection.update_one({"_id": server_ID}, {"$set": document})
 
                         await self.play_next(vc, find_document(server_ID), server_ID)  # move next
                     else:
@@ -90,22 +89,29 @@ class SkipAndRem_cog(commands.Cog):
     async def remove(self, ctx, *args):
         query = " ".join(args)
         num_queue = int(query)
+        server_ID = ctx.message.guild.id
+        document = find_document(server_ID)
+        collection = music_sessions()
+        vc = discord.utils.get(self.bot.voice_clients, guild=ctx.guild)
+        music_queue = document.get("music_queue", [])
 
-        if num_queue > len(self.music_queue):
+        if num_queue > len(music_queue):
             await ctx.send("Does not exist in queue!!!")
 
-        elif self.vc is not None and self.is_Playing:
-            if num_queue <= len(self.music_queue):
+        elif vc is not None and document['is_Playing']:
+            if num_queue <= len(music_queue):
                 voice_channel = ctx.author.voice.channel
                 num_members = len(voice_channel.members) - 1  # account for bot
                 num_success_vote = (num_members // 2) + 1
 
                 if num_members <= 2:
-                    removed = self.music_queue[num_queue - 1][0]['title']
-                    self.music_queue.pop(num_queue - 1)
+                    removed = music_queue[num_queue - 1]['title']
+                    music_queue.pop(num_queue - 1)
+                    collection.update_one({"_id": server_ID}, {"$set": {"music_queue": music_queue}})
+
                     await ctx.send(f"The song: **__{removed}__** was removed from queue!!!")
                 elif num_members > 2:
-                    removed = self.music_queue[num_queue - 1][0]['title']
+                    removed = music_queue[num_queue - 1]['title']
                     message = await ctx.send(f"Should **__{removed}__** be removed? 15 seconds to vote")
                     await message.add_reaction('\u2705')
                     await message.add_reaction('\u274C')
@@ -132,9 +138,10 @@ class SkipAndRem_cog(commands.Cog):
                                 break
 
                         if Check_Count >= num_success_vote:
-                            removed = self.music_queue[num_queue - 1][0]['title']
-                            user_id = self.music_queue[num_queue - 1][0]['queuer']
-                            self.music_queue.pop(num_queue - 1)
+                            removed = music_queue[num_queue - 1]['title']
+                            user_id = music_queue[num_queue - 1]['queuer']
+                            music_queue.pop(num_queue - 1)
+                            collection.update_one({"_id": server_ID}, {"$set": {"music_queue": music_queue}})
 
                             await ctx.send(f"The song: **__{removed}__** has been skipped.\n"
                                            f" Who put this shit song here? \n"
@@ -143,6 +150,8 @@ class SkipAndRem_cog(commands.Cog):
                             await ctx.send("The song has not been removed. Not enough votes!!!")
                     except asyncio.TimeoutError:
                         await ctx.send("No votes were cast within 15 seconds. The song will not be skipped!!!")
+
+
             else:
                 await ctx.send("Can't be skipped if it doesn't exist!!!")
         else:
